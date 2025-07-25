0dd4f80c9db3ef78ba55d09e265c30f583e42859
feerozmandiha <feerozmandiha@gmail.com>
2025-07-25 16:29:10 +0330

    اولین افزونه برای ساخت بلوک سفارشی گوتنبرگ

---
+++ xennic-block.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: بلوکهای زننیک
+ * Description: افزونه ساخت بلوکهای سفارشی گوتنبرگ برای قالب Xennic Theme
+ * Version: 1.0.0
+ * Author:  تیم زننیک
+ */
+
+defined('ABSPATH') || exit;
+
+// بار گذاری فایل های مورد نیاز افزونه
+
+require_once plugin_dir_path(__FILE__) . 'src/init.php';
